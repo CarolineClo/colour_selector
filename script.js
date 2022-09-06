dceeba60f7228598764abf01d0c488ed16412e25
caroline cloughley <caro672g@stud.kea.dk>
2022-09-06 14:44:05 +0200

    hex and rgb works here

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
 
 // let value = document.getElementById("colour_selector").value;
 let colourSelector;
-
-window.addEventListener("DOMContentLoaded", start, false);
+window.addEventListener("DOMContentLoaded", start);
 
 function start() {
   colourSelector = document.querySelector("#colour_selector");
@@ -12,4 +11,16 @@ function start() {
 
 function updateHex(event) {
   document.querySelector(".hex").textContent = `HEX: ${event.target.value}`;
+  let hex = event.target.value;
+  console.log(hex);
+  getRGB(hex);
+}
+
+function getRGB(hex) {
+  let r = parseInt(hex.substring(1, 3), 16);
+  let g = parseInt(hex.substring(3, 5), 16);
+  let b = parseInt(hex.substring(5, 7), 16);
+  console.log(`${r}, ${g}, ${b}`);
+  //show it
+  document.querySelector(".rgb").textContent = `RGB: ${r}, ${g}, ${b}`;
 }
